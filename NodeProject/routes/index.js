b10a31e0fabee1c097404ba34b21d79cf529c995
Kim Dae Hee <anyozcall@naver.com>
2017-10-13 16:03:59 +0900

    10.13
    Vue.JS를 이용한 Node.js Project 및 Mysql settings

--- NodeProject/routes/index.js
+++ NodeProject/routes/index.js
@@ -1,18 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database-module/database.js'); // mysql 연동 
+
+router.get('/', function (req, res, next) {
+    
+        db.getPool().getConnection(function (err, connection) {
+            
+            
+            // Use the connection
+            connection.query('SELECT * FROM board limit 1', function (err, rows) {
+                if (err) console.error("err : " + err);
+                console.log("rows : " + JSON.stringify(rows));
+                res.render('index', {title: 'test', rows: rows});
+                connection.release();
+                console.log("종료 확인");
+                // Don't use the connection here, it has been returned to the pool.
+            });
+    
+    
+        });
+    });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var title = req.query.title;
-  res.render('index', { title: title });
+router.get('/get', function (req, res, next) {
+
+    db.getPool().getConnection(function (err, connection) {
+        
+        
+        // Use the connection
+        connection.query('SELECT * FROM board limit 1', function (err, rows) {
+            if (err) console.error("err : " + err);
+            console.log("rows : " + JSON.stringify(rows));
+            res.json({rows: rows});
+            connection.release();
+            console.log("종료 확인");
+            // Don't use the connection here, it has been returned to the pool.
+        });
+
+
+    });
 });
-
-/* POST home page */
-router.post('/', function(req, res, next) {
-  var title = req.body.title;
-  res.render('index', { title: 'Express' });
-});
-
-
 
 module.exports = router;
